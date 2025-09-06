6119894bd4823fc30ffe62efd2ca15c89dd83f96
afingpo <han_mingp0@163.com>
2025-09-06 22:23:51 +0800

    2025-09-06

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "涵哲子",
 	bio: "天哲地理，共公卿好",
 	links: [
